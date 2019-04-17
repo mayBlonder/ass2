5d320358156f41eb478b4c6b9b2fda86dbd78c68
jubelsM <44232197+jubelsM@users.noreply.github.com>
2019-04-17 18:51:49 +0300

    notes

--- assignment2/src/FloorsArrayList.java
+++ assignment2/src/FloorsArrayList.java
@@ -24,46 +24,110 @@ public class FloorsArrayList implements DynamicSet {
     
     
     private FloorsArrayLink find(double key){
-    	return null;
+    	int i=1;
+    	FloorsArrayLink current=this.first;
+    	while(i<=current.getArrSize()&&current.getNext(i).getKey()<=key)
+    		{
+    			i++;
+    		}
+    	current=current.getNext(i-1);
+    	i=current.getArrSize()-1;
+    	while(current.getKey()<=key)
+    	{
+    		if(current.getNext(i).getKey()<=key)
+    			current=current.getNext(i);
+    		else
+    			i--;
+    	}
+      	return current;
     }
     
     @Override
     public void insert(double key, int arrSize) {
-        //@TODO: implement
+    	FloorsArrayLink add=new FloorsArrayLink(key,arrSize);
+    	FloorsArrayLink prev=find(key);
+    	FloorsArrayLink next=prev.getNext(1);
+    	int i=1;
+    	while(i<arrSize)
+    	{
+    		while(i<=prev.getArrSize()&i<arrSize)
+    		{
+    			prev.setNext(i,add);
+    			i++;
+    		}
+    		prev=prev.getPrev(prev.getArrSize());
+    		
+    	}
+    	i=1;
+    	while(i<arrSize)
+    	{
+    		while(i<=next.getArrSize()&i<arrSize)
+    		{
+    			next.setPrev(i,add);
+    			i++;
+    		}
+    		next=next.getNext(next.getArrSize());
+    		
+    	}
     }
 
     @Override
     public void remove(FloorsArrayLink toRemove) {
-        //@TODO: implement
+    	FloorsArrayLink prev=toRemove.getPrev(1);
+    	FloorsArrayLink next=toRemove.getNext(1);
+    	int i=1;
+    	while(i<toRemove.getArrSize())
+    	{
+    		while(i<=prev.getArrSize()&i<toRemove.getArrSize())
+    		{
+    			prev.setNext(i,toRemove.getNext(i));
+    			i++;
+    		}
+    		prev=prev.getPrev(prev.getArrSize());
+    		
+    	}
+    	i=1;
+    	while(i<toRemove.getArrSize())
+    	{
+    		while(i<=next.getArrSize()&i<toRemove.getArrSize())
+    		{
+    			next.setPrev(i,toRemove.getPrev(i));
+    			i++;
+    		}
+    		next=next.getNext(next.getArrSize());
+    		
+    	}
     }
 
     @Override
     public FloorsArrayLink lookup(double key) {
-        //@TODO: implement
-        return null;
+    	FloorsArrayLink current=find(key);
+    	if(current.getKey()==key)
+        	return current;
+    	return null;
     }
 
     @Override
     public double successor(FloorsArrayLink link) {
-        //@TODO: implement
-        return 0;
+    	if(link.getNext(1)==last)
+		return first.getKey();
+    return link.getNext(1).getKey();
     }
 
     @Override
     public double predecessor(FloorsArrayLink link) {
-        //@TODO: implement
-        return 0;
+        if(link.getPrev(1)==first)
+			return last.getKey();
+        return link.getPrev(1).getKey();
     }
 
     @Override
     public double minimum() {
-        //@TODO: implement
-        return 0;
+        return first.getNext(1).getKey();
     }
 
     @Override
     public double maximum() {
-        //@TODO: implement
-        return 0;
+        return last.getPrev(1).getKey();
     }
 }
